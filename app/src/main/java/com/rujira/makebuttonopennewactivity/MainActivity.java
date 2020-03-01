8520b3397d9c40212dc832eadea1053ef76aa4c4
Rujira Kongsomran <biwberry@outlook.com>
2020-03-01 12:24:16 +0700

    Create string and int variable for receive value from EditText 1 and EditText 2

--- app/src/main/java/com/rujira/makebuttonopennewactivity/MainActivity.java
+++ app/src/main/java/com/rujira/makebuttonopennewactivity/MainActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnOpenActivity;
+    EditText et1;
+    EditText et2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +23,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initInstances() {
-        btnOpenActivity = (Button) findViewById(R.id.btnOpenActivity);
+        et1 = (EditText) findViewById(R.id.et1);
+        et2 = (EditText) findViewById(R.id.et2);
 
+        btnOpenActivity = (Button) findViewById(R.id.btnOpenActivity);
         btnOpenActivity.setOnClickListener(btnOpenActivityListener);
+
     }
 
     View.OnClickListener btnOpenActivityListener = new View.OnClickListener() {
@@ -35,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private void openSecondActivity() {
+        String text = et1.getText().toString();
+        int number = Integer.parseInt(et2.getText().toString());
+
         Intent intent = new Intent(this, SecondActivity.class);
         startActivity(intent);
 
